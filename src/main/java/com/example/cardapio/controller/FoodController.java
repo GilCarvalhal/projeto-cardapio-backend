2c9d8d7b54b4074db2461dbc86aa99cf41340a39
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-08-18 13:49:25 -0300

    pt1 desenvolvimento do backend

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,40 @@
+package com.example.cardapio.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
